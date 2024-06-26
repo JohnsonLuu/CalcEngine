4486cb77f39f52e0bee652bf9a916cbca2358440
Johnson <johnsonluu1@gmail.com>
2024-06-26 08:58:41 -0500

    Learning StringBuilder

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -1,5 +1,8 @@
 package com.pluralsight.calcengine;
 
+import java.io.OutputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String [] args){
@@ -15,7 +18,11 @@ public class Main {
 
             for(double currentResult : results)
                 System.out.println(currentResult);
-        } else if (args.length == 3){
+        }
+        else if (args.length == 1 && args[0].equals("interactive")){
+            executeInteractively();
+        }
+        else if (args.length == 3){
             handleCommandLine(args);
         }
         else{
@@ -23,6 +30,52 @@ public class Main {
         }
     }
 
+    static void executeInteractively(){
+        System.out.println("Enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displayResult(opCode, leftVal, rightVal, result);
+    }
+
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
+        char symbol = symbolFromOpCode(opCode);
+        StringBuilder builder = new StringBuilder(20);
+        // we have integers, char and string literals, which StringBuilder can handle
+        builder.append(leftVal);
+        builder.append(" ");
+        builder.append(symbol);
+        builder.append(" ");
+        builder.append(rightVal);
+        builder.append(" = ");
+        builder.append(result);
+        String output = builder.toString();
+        System.out.println(output);
+
+    }
+
+    private static char symbolFromOpCode(char opCode){
+        // loop through opCodes array to find associated symbol
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+
+        for(int index = 0; index < opCodes.length; index++){
+            if(opCode == opCodes[index]){
+                symbol = symbols[index];
+                // no need to waste iterations after finding the symbol
+                break;
+            }
+        }
+        return symbol;
+    }
     private static void handleCommandLine(String[] args) {
         char opCode = args[0].charAt(0);
         double leftVal = Double.parseDouble(args[1]);
@@ -45,5 +98,24 @@ public class Main {
         };
 
         return result;
+    }
+    static char opCodeFromString(String operationName){
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word){
+        String [] numberWords = {
+                "zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"
+        };
+        double value = 0d;
+        for(int index = 0; index < numberWords.length; index++){
+            if(word.equals(numberWords[index])){
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 }
